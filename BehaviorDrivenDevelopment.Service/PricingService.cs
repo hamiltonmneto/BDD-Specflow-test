3978b2571a1105bdf37e9ede02148a7b347a5c4b
Hamilton Monteiro Neto <hamiltonmneto@gmail.com>
2021-06-06 23:18:36 -0300

    Behavior Driven Development test example

---
+++ BehaviorDrivenDevelopment.Service/PricingService.cs
@@ -0,0 +1,49 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+using static BehaviorDrivenDevelopment.Service.PricingService;
+
+namespace BehaviorDrivenDevelopment.Service
+{
+    public interface IPricingService
+    {
+        decimal GetBasketTotalAmount(Basket basket);
+    }
+    public class PricingService : IPricingService
+    {
+        public decimal GetBasketTotalAmount(Basket basket)
+        {
+            if (!basket.Products.Any())
+            {
+                return 0;
+            }
+
+            var basketValue = basket.Products.Sum(x => x.Price);
+
+            if (basket.User.IsLoggedIn)
+            {
+                return basketValue - (basketValue * 0.05m);
+            }
+
+            return basketValue;
+        }
+
+        public class Basket
+        {
+            public User User { get; set; }
+            public List<Product> Products { get; } = new List<Product>();
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        public class User
+        {
+            public bool IsLoggedIn { get; set; }
+        }
+    }
+}
---
+++ BehaviorDrivenDevelopment.Tests.Behavior/Steps/LoggedInDiscountSteps.cs
@@ -0,0 +1,56 @@
+﻿using BehaviorDrivenDevelopment.Service;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using static BehaviorDrivenDevelopment.Service.PricingService;
+
+namespace BehaviorDrivenDevelopment.Tests.Behavior.Steps
+{
+    [Binding]
+    public class LoggedInUsersHasDiscountSteps
+    {
+        private User _user;
+        private Basket _basket;
+        private readonly IPricingService _pricingService = new PricingService();
+
+        [Given(@"a user that is not logged in")]
+        public void GivenAUserThatIsNotLoggedIn()
+        {
+            _user = new User { IsLoggedIn = false };
+        }
+
+        [Given(@"an empty basket")]
+        public void GivenAnEmptyBasket()
+        {
+            _basket = new Basket { 
+                User = _user
+            };
+        }
+        
+        [Given(@"a user that is logged in")]
+        public void GivenAUserThatIsLoggedIn()
+        {
+            _user = new User { IsLoggedIn = true };
+
+        }
+
+        [When(@"a (.*) that costs (.*)GBP is added tp the basket")]
+        public void WhenAT_ShirtThatCostsGBPIsAddedTpTheBasket(string itemName, decimal price)
+        {
+            _basket.Products.Add(new Product {
+                Name = itemName,
+                Price = price
+            });
+            
+        }
+       
+        
+        [Then(@"the basket value is (.*) GBP")]
+        public void ThenTheBasketValueIsGBP(decimal expectedBasketValue)
+        {
+            var basketValue = _pricingService.GetBasketTotalAmount(_basket);
+            basketValue.Should().Be(expectedBasketValue);
+        }
+    }
+}
